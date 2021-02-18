85e392f8f145a20b0056edd68a9432c65dff48c6
DCTorres87 <cruz_danielle_torres@yahoo.com>
2021-02-17 22:34:38 -0500

    working on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: 'JohnDoe', password: 'blueboat');
+User.destroy_all;
+user = User.create(name: 'JohnDoe', password: 'blueboat');
+
+# Note.create(category_id: 1, user_id: 1);
